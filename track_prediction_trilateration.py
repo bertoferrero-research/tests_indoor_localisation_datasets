3fd1d3091f416d09e7729dbb0488d441f68fd596
Berto Ferrero <berto@prestaquality.com>
2023-07-12 10:18:04 +0200

    Primeros pasos con trilateracion, falta revisar la ecuación

--- track_prediction_trilateration.py
+++ track_prediction_trilateration.py
@@ -1,5 +1,7 @@
 from lib.datasethelper import parseDevices
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Configuración
 input_file_name = 'track_straight_01_all_sensors.mbd_window'
@@ -38,33 +40,37 @@ def rssiToDistance(int: RSSI, int txPower) {
    return pow(10, ((double) (txPower - RSSI - PL0)) / (10 * 2));
 }'''
 
-def trilateration(nodo1, nodo2, nodo3, rssi1, rssi2, rssi3):
-    # Posiciones de los nodos (x, y)
-    x1, y1 = nodo1
-    x2, y2 = nodo2
-    x3, y3 = nodo3
+def trilateration_min_cuad(nodos):
+    # Convertir RSSI a distancia
+    #Basado en el método https://medium.com/beingcoders/convert-rssi-value-of-the-ble-bluetooth-low-energy-beacons-to-meters-63259f307283
+    distancias = [(10 ** ((-69 -(nodo[1]))/(10 * 2))) for nodo in nodos]  # Convertir RSSI a distancia
 
-    # Convertir RSSI a distancia utilizando un modelo de pérdida de trayectoria libre (FSPL)
-    distancia1 = 10 ** ((27.55 - rssi1) / 20)
-    distancia2 = 10 ** ((27.55 - rssi2) / 20)
-    distancia3 = 10 ** ((27.55 - rssi3) / 20)
+    # Número de nodos y dimensiones (x, y)
+    num_nodos = len(nodos)
+    dimension = nodos[0][0].shape[0]
 
-    # Trilateración
-    A = 2 * x2 - 2 * x1
-    B = 2 * y2 - 2 * y1
-    C = distancia1**2 - distancia2**2 - x1**2 + x2**2 - y1**2 + y2**2
-    D = 2 * x3 - 2 * x2
-    E = 2 * y3 - 2 * y2
-    F = distancia2**2 - distancia3**2 - x2**2 + x3**2 - y2**2 + y3**2
+    # Matrices para el sistema de ecuaciones
+    A = np.zeros((num_nodos - 1, dimension))
+    b = np.zeros((num_nodos - 1, 1))
 
-    x = (C*E - F*B) / (E*A - B*D)
-    y = (C*D - A*F) / (B*D - A*E)
+    # Calcular las diferencias de distancia
+    for i in range(1, num_nodos):
+        A[i-1, :] = 2 * (nodos[i][0] - nodos[0][0])
+        b[i-1] = np.square(distancias[0]) - np.square(distancias[i]) - \
+                  np.dot(nodos[i][0], nodos[i][0]) + np.dot(nodos[0][0], nodos[0][0])
 
-    return x, y
+    # Resolver el sistema de ecuaciones
+    x = np.linalg.lstsq(A, b, rcond=None)[0]
+
+    # Calcular la posición estimada
+    posicion_estimada = nodos[0][0] + x.flatten()
+
+    return posicion_estimada
 
 #Cargamos el csv de la trayectoria
 trajectory = pd.read_csv(track_file)
 rssis = trajectory.iloc[:,4:]
+output_data = []
 for index, rssi_row in rssis.iterrows():
   #Obtenemos los tres mejores dongles (con el valor rssi más alto)
   three_best_dongles = rssi_row.sort_values(ascending=False).head(3).index.tolist()
@@ -72,8 +78,9 @@ for index, rssi_row in rssis.iterrows():
   #Localizamos cada dongle en el listado cargado y acumulamos su posición  
   positions = []
   for dongleMac in three_best_dongles:
+    dongle_data = []
     #Extraemos la posición
-    dongle_data = dongles[dongleMac][0][:2]
+    dongle_data.append(np.array(dongles[dongleMac][0][:2]))
     #Extraemos el rssi
     dongle_rssi = rssi_row[dongleMac]
     #Añadimos la posición a la lista
@@ -82,11 +89,45 @@ for index, rssi_row in rssis.iterrows():
     positions.append(dongle_data)
   
   #Calculamos la trilateración
-  x, y = trilateration(positions[0][:2], positions[1][:2], positions[2][:2], positions[0][2], positions[1][2], positions[2][2])
+  #x, y = trilateration(positions[0][:2], positions[1][:2], positions[2][:2], positions[0][2], positions[1][2], positions[2][2])
+  position = trilateration_min_cuad(positions)
+  output_data.append({
+    'predicted_x': position[0],
+    'predicted_y': position[1],
+    #'predicted_z': predictions[index][2],
+    'real_x': trajectory.iloc[index, 1],
+    'real_y': trajectory.iloc[index, 2],
+    #'real_z': output_data[index][2],
+  })
+
+output_data = pd.DataFrame(output_data)
+#Preparamos cálculos
+output_data['deviation_x'] = (output_data['predicted_x'] - output_data['real_x']).abs()
+output_data['deviation_y'] = (output_data['predicted_y'] - output_data['real_y']).abs()
+output_data['eclidean_distance'] = np.sqrt(np.power(output_data['deviation_x'], 2) + np.power(output_data['deviation_y'], 2))
+
+#Imprimimos la desviacion máxima minima y media de X e Y
+print("- Desviaciones en predicciones -")
+print("Desviación máxima X: "+str(output_data['deviation_x'].max()))
+print("Desviación mínima X: "+str(output_data['deviation_x'].min()))
+print("Desviación media X: "+str(output_data['deviation_x'].mean()))
+print("Desviación máxima Y: "+str(output_data['deviation_y'].max()))
+print("Desviación mínima Y: "+str(output_data['deviation_y'].min()))
+print("Desviación media Y: "+str(output_data['deviation_y'].mean()))
+print("Distancia euclídea máxima: "+str(output_data['eclidean_distance'].max()))
+print("Distancia euclídea mínima: "+str(output_data['eclidean_distance'].min()))
+print("Distancia euclídea media: "+str(output_data['eclidean_distance'].mean()))
 
 
-  print(x,y)
-  break
+#Hacemos la salida
+output_data.to_csv(output_file, index=False)
+
+
+#Mostramos el grafico
+plt.plot([0, 0, dim_y, dim_y, 0], [0, dim_x,  dim_x, 0, 0], 'go-', label='Real', linewidth=1)
+plt.plot(output_data['real_y'].values, output_data['real_x'].values, 'ro-', label='Real', linewidth=1)
+plt.plot(output_data['predicted_y'].values, output_data['predicted_x'].values, 'mo-', label='Calculada', linewidth=1)
+plt.show()
 
 
 '''
