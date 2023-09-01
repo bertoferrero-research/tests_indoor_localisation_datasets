d353f7a09b0506f80f43525559668c8b310ed72a
Berto Ferrero <berto@prestaquality.com>
2023-09-01 08:53:40 +0200

    Autoencoder de trayectorias

---
+++ 03-trayectorias/autoencoder/autoencoder.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import math
+import os.path
+import pickle
+import random
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import  Model
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, UpSampling1D, Input
+import sys
+script_dir = os.path.dirname(os.path.abspath(__file__)) #Referencia al directorio actual, por si ejecutamos el python en otro directorio
+root_dir = script_dir+'/../../'
+sys.path.insert(1, root_dir)
+from lib.trainingcommon import load_data
+from lib.trainingcommon import plot_learning_curves
+
+#Variables globales
+track_file = root_dir+'/preprocessed_inputs/fingerprint_history_window_median.csv'
+scaler_file = script_dir+'/files/scaler.pkl'
+model_file = script_dir+'/files/model.h5'
+random_seed = 42
+
+#Hiperparámetros
+embedding_size = 12
+batch_size = 1250
+epochs = 75
+loss = 'mse' #'mse'
+optimizer = 'RMSProp'
+
+#Cargamos la semilla de los generadores aleatorios
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+# ---- Preparación de los datos ---- #
+X, y = load_data(track_file, scaler_file, train_scaler_file=True)
+# Cambiamos la forma de los datos para que sean compatibles con el modelo
+X = X.values.reshape(X.shape[0], X.shape[1], 1)
+# En este caso X e y son iguales por lo que la y la descartamos
+X_train, X_test, y_train, y_test = train_test_split(X, X, test_size=0.2, random_state=random_seed)
+
+
+# ---- Construcción del modelo ---- #
+
+#CNN
+# Entrada
+input_sensors = Input(shape=(X.shape[1], 1)) 
+# Encoder...
+x = Conv1D(64, 3, activation='relu', padding='same')(input_sensors)
+x = MaxPooling1D(2, padding='same')(x)
+x = Conv1D(128, 2, activation='relu', padding='same')(x)
+x = MaxPooling1D(2, padding='same')(x)
+x = Conv1D(256, 2, activation='relu', padding='same')(x)
+encoded = MaxPooling1D(1, padding='same')(x)
+
+# Decoder...
+x = Conv1D(256, 2, activation='relu', padding='same')(encoded)
+x = UpSampling1D(1)(x)
+x = Conv1D(128, 2, activation='relu', padding='same')(x)
+x = UpSampling1D(2)(x)
+x = Conv1D(64, 3, activation='relu', padding='same')(x)
+x = UpSampling1D(2)(x)
+
+
+# Capa de salida con 1 convolución
+decoded = Conv1D(1, 3, activation='linear', padding='same')(x)
+
+# Compilamos y entrenamos
+autoencoder = Model(inputs=input_sensors, outputs=decoded)
+encoder = Model(inputs=input_sensors, outputs=encoded)
+autoencoder.compile(optimizer='RMSProp', loss='mse', metrics=['mse'])
+
+history = autoencoder.fit(X_train, y_train, epochs=40, batch_size=1250, validation_data=(X_test, y_test))
+
+# Guardamos el modelo
+encoder.save(model_file)
+
+# ---- Evaluación del modelo ---- #
+autoencoder.summary()
+encoder.summary()
+score = autoencoder.evaluate(X_test, y_test, verbose=0)
+print('Resultado en el test set:')
+print('Test loss: {:0.4f}'.format(score[0]))
+print('Test accuracy: {:0.2f}%'.format(score[1] * 100))
+plot_learning_curves(history)
